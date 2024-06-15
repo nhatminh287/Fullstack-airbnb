6bc3246dc492f6f8c59cbfbb60a0b097df7e2701
nhatminh287 <nhatminhpro2001@gmail.com>
2024-06-15 10:32:27 +0700

    config deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'res.cloudinary.com'],
       },
+      experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
 
 export default nextConfig;
